c77dac1f62afd3e22cc5d423ca824298205b394f
Akachai Kaning <akachai.k@taskworld.com>
2014-11-03 18:43:15 +0700

    Assignment 1

---
+++ app.js
@@ -0,0 +1,19 @@
+var stdio = require('stdio');
+var math = require('mathjs');
+
+stdio.question('Enter a Number to find factorial  ', function(err, num)
+{
+	if(num != parseInt(num, 10)) 
+	{
+		console.log('Please enter an integer');
+		return;
+	}
+	if(num < 0)
+	{
+		console.log('Please enter only a positive number');
+		return;
+	}
+	console.log(math.factorial(parseInt(num)));
+});
+
+
