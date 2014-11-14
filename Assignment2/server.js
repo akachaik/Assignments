5d15169215cc190a7ca3f0f418a385bd7495d402
Akachai Kaning <akachai.k@taskworld.com>
2014-11-14 20:25:57 +0700

    Assignment 7

--- Assignment2/server.js
+++ Assignment2/server.js
@@ -1,11 +1,73 @@
 var hapi = require('hapi');
 var path = require('path');
+var Joi = require('joi');
+
+var Mongoose = require('mongoose');
+
+Mongoose.connect('mongodb://localhost/registerdb');
+var db = Mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+       
+db.once('open', function callback() {
+    console.log("Connection with database succeeded.");
+});
+
+ var registerSchema = new Mongoose.Schema({
+           title : { type: String },
+           firstName : { type: String },
+           lastName : { type: String },
+           gender : { type: String },
+           other:  { type: String },
+           favoritePet: []
+       });
+var MemberModel = Mongoose.model('member', registerSchema, 'members');
 
 var server = new hapi.Server(3000);
 
 server.views({
 	engines: { html: require('handlebars') },
 	path: path.join(__dirname, 'templates')
+});
+
+server.route({
+	method: 'GET',
+	path: '/register',
+	handler: function(requres, reply) {
+		reply.view('register', {title: 'Assignment 7 : Assignment 7: Hapi + Joi + Mongoose'});
+	}
+});
+
+server.route({
+	method: 'POST',
+	path: '/register',
+	handler: function(request, reply) {
+		console.log("post");
+		if (request.payload) {
+			var memberModel = new MemberModel(request.payload);
+
+           memberModel.save(function (err , newMember) {
+               if (err) {
+                   throw err;
+               } else {
+                   console.log('save -> ', newMember);
+                   reply(newMember);
+               }
+           });
+
+		}
+	}
+	,
+	config: {
+		 validate: {
+           payload : {
+               title : Joi.string().required(),
+               firstName : Joi.string().required(),
+               lastName : Joi.string().required(),
+               other : Joi.string().required(),
+               favoritePet : Joi.string().required()
+           }
+		}
+	}
 });
 
 server.route({
